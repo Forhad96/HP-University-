9d0ed59fb77b10a0d4a6b0eeb27abc0e264b75bd
Forhad96 <forhadairdrop@gmail.com>
2024-06-02 18:27:43 +0200

    transtion rollback

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -7,17 +7,19 @@ import { StudentModel } from '../student/student.model';
 import { TUser } from './user.interface';
 import { UserModel } from './user.model';
 import { generateStudentId } from './user.utils';
+import mongoose from 'mongoose';
+
 const createStudent = async (password: string, payload: TStudent) => {
   // create a user object
   const userData: Partial<TUser> = {};
 
-  //if password is not given , use default password
+  // if password is not given, use default password
   userData.password = password || (config.default_password as string);
 
-  //set student role
+  // set student role
   userData.role = 'student';
 
-  //find academic semester information
+  // find academic semester information
   const academicSemester = await academicSemesterModel.findById(
     payload.academicSemester,
   );
@@ -26,20 +28,33 @@ const createStudent = async (password: string, payload: TStudent) => {
     throw new AppError(httpStatus.NOT_FOUND, 'Academic semester not found');
   }
 
-  //set generated it
-  userData.id = await generateStudentId(academicSemester);
+  const session = await mongoose.startSession();
+  try {
+    session.startTransaction();
 
-  // create a user
-  const newUser = await UserModel.create(userData);
+    // set generated id
+    userData.id = await generateStudentId(academicSemester);
 
-  //create a student
-  if (Object.keys(newUser).length) {
-    // set id , _id as user
-    payload.id = newUser.id;
-    payload.user = newUser._id; //reference _id
+    // create a user
+    const newUser = await UserModel.create([userData], { session });
 
-    const newStudent = await StudentModel.create(payload);
+    // create a student
+    if (!newUser.length) {
+      throw new AppError(httpStatus.BAD_REQUEST, 'Failed to create user');
+    }
+    // set id, _id as user
+    payload.id = newUser[0].id;
+    payload.user = newUser[0]._id; // reference _id
+
+    const newStudent = await StudentModel.create([payload], { session });
+
+    await session.commitTransaction();
+    await session.endSession();
     return newStudent;
+  } catch (err) {
+    await session.abortTransaction();
+    session.endSession();
+    throw err; // rethrow the error after cleanup
   }
 };
 
