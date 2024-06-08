84a4a85f8fad47fde59905c48121bd26c712f5ae
Forhad96 <forhadairdrop@gmail.com>
2024-06-08 17:52:53 +0200

    Update:Create academic semester registation  logic for status checking

--- src/app/modules/semesterRegistration/semesterRegistration.service.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.service.ts
@@ -6,7 +6,25 @@ import { SemesterRegistrationModel } from './semesterRegistration.model';
 import QueryBuilder from '../../builder/QueryBuilder';
 
 const createSemesterRegistration = async (payload: TSemesterRegistration) => {
+  /**
+   * Step1: Check if there any registered semester that is already 'UPCOMING'|'ONGOING'
+   * Step2: Check if the semester is exist
+   * Step3: Check if the semester is already registered!
+   * Step4: Create the semester registration
+   */
   const academicSemesterId = payload.academicSemester;
+  //Check if there any registered semester that is already 'UPCOMING'|'ONGOING'
+  const isThereAnyUpcomingOrOngoingSemester =
+    await SemesterRegistrationModel.findOne({
+      $or: [{ status: 'UPCOMING' }, { status: 'ONGOING' }],
+    });
+
+  if (isThereAnyUpcomingOrOngoingSemester) {
+    throw new AppError(
+      httpStatus.BAD_REQUEST,
+      `There is already and ${isThereAnyUpcomingOrOngoingSemester.status} registered semester`,
+    );
+  }
   //check if the semester already exists or not
   const isSemesterExist =
     await academicSemesterModel.findById(academicSemesterId);
@@ -33,15 +51,15 @@ const createSemesterRegistration = async (payload: TSemesterRegistration) => {
 
 const getAllSemesterRegistrations = async (query: Record<string, unknown>) => {
   const semesterRegistrationQuery = new QueryBuilder(
-    SemesterRegistrationModel.find().populate("academicSemester"),
+    SemesterRegistrationModel.find().populate('academicSemester'),
     query,
   )
     .filter()
     .pagination()
     .fields();
 
-    const result = await semesterRegistrationQuery.modelQuery;
-    return result;
+  const result = await semesterRegistrationQuery.modelQuery;
+  return result;
 };
 
 const getSingleSemesterRegistration = async (id: string) => {
